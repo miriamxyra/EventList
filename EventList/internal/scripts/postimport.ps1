f3a7d80135385db35c29b3e88f03735a0c598c02
Miriam Wiesner <miriamxyra@users.noreply.github.com>
2020-10-01 12:25:38 +0200

    Renewed postimport.ps1

--- EventList/internal/scripts/postimport.ps1
+++ EventList/internal/scripts/postimport.ps1
@@ -1,17 +1,26 @@
-﻿# Add all things you want to run after importing the main code
+﻿<#
+Add all things you want to run after importing the main function code
+
+WARNING: ONLY provide paths to files!
+
+After building the module, this file will be completely ignored, adding anything but paths to files ...
+- Will not work after publishing
+- Could break the build process
+#>
+
+$moduleRoot = Split-Path (Split-Path $PSScriptRoot)
 
 # Load Configurations
-foreach ($file in (Get-ChildItem "$ModuleRoot\internal\configurations\*.ps1" -ErrorAction Ignore)) {
-	. Import-ModuleFile -Path $file.FullName
-}
+(Get-ChildItem "$moduleRoot\internal\configurations\*.ps1" -ErrorAction Ignore).FullName
+
+# Load Scriptblocks
+(Get-ChildItem "$moduleRoot\internal\scriptblocks\*.ps1" -ErrorAction Ignore).FullName
 
 # Load Tab Expansion
-foreach ($file in (Get-ChildItem "$ModuleRoot\internal\tepp\*.tepp.ps1" -ErrorAction Ignore)) {
-	. Import-ModuleFile -Path $file.FullName
-}
+(Get-ChildItem "$moduleRoot\internal\tepp\*.tepp.ps1" -ErrorAction Ignore).FullName
 
 # Load Tab Expansion Assignment
-. Import-ModuleFile -Path "$ModuleRoot\internal\tepp\assignment.ps1"
+"$moduleRoot\internal\tepp\assignment.ps1"
 
 # Load License
-. Import-ModuleFile -Path "$ModuleRoot\internal\scripts\license.ps1"
+"$moduleRoot\internal\scripts\license.ps1"
